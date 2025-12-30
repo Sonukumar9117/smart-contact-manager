9c2abbcf96e9bc0b0b04bc48e39faab2d053ef32
Sonukumar9117 <krsonu1791@gmail.com>
2025-12-31 00:22:03 +0530

    project-started

---
+++ src/main/java/com/scm/scm/controller/Home.java
@@ -0,0 +1,28 @@
+package com.scm.scm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Home {
+    
+    @RequestMapping("/home")
+    public String home(Model model){
+        model.addAttribute("title", "Smart Contact Manager");
+        model.addAttribute("message", "Welcome to Smart Contact Manager - Manage your contacts efficiently!");
+        model.addAttribute("name", "Sonu Kumar");
+        model.addAttribute("linkedin","https://www.linkedin.com/in/sonu-kumar-81284b230/");
+        return "home";
+    }
+    @RequestMapping("/about")
+    public String about(Model model){
+        model.addAttribute("title", "About Smart Contact Manager");
+        return "about";
+    }
+    @RequestMapping("/service")
+    public String services(Model model){
+        model.addAttribute("title", "Services");
+        return "service";
+    }
+}
